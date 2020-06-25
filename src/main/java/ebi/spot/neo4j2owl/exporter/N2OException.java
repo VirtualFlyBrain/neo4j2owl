634bda1268fca1349029c5d502a14a22da765f8f
matentzn <nicolas.matentzoglu@gmail.com>
2020-06-26 00:35:29 +0300

    removed unnecessary import from N2OException

--- src/main/java/ebi/spot/neo4j2owl/exporter/N2OException.java
+++ src/main/java/ebi/spot/neo4j2owl/exporter/N2OException.java
@@ -1,7 +1,5 @@
 package ebi.spot.neo4j2owl.exporter;
 
-import org.apache.log4j.spi.ErrorCode;
-
 public class N2OException extends Exception {
     public N2OException(String message, Throwable cause) {
         super(message, cause);
