fcf31fea2ae7377138c45a9bc9124a1603b6b300
matentzn <nicolas.matentzoglu@gmail.com>
2020-06-25 12:47:33 +0300

    Refactored tests

--- src/test/java/ebi/spot/neo4j2owl/N2OProcedureTest.java
+++ src/test/java/ebi/spot/neo4j2owl/N2OProcedureTest.java
@@ -14,12 +14,20 @@ import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by jbarrasa on 21/03/2016.
  */
 public class N2OProcedureTest {
+
+
+    private static String test_resources_web = "https://raw.githubusercontent.com/VirtualFlyBrain/neo4j2owl/master/src/test/resources/";
 
 
 
@@ -30,10 +38,20 @@ public class N2OProcedureTest {
     }
 
     @Test
-    public void owl2ImportSmall() throws Exception {
-        String ontologyUrl = "https://raw.githubusercontent.com/matentzn/ontologies/master/smalltest.owl";
-        String configUrl = "https://raw.githubusercontent.com/VirtualFlyBrain/vfb-prod/master/neo4j2owl-config.yaml";
+    public void owl2ImportFromWeb() throws Exception {
+        String ontologyUrl = test_resources_web+"smalltest.owl";
+        String configUrl = test_resources_web+"smalltest-config.yaml";
+        runSmallTest(ontologyUrl,configUrl);
+    }
 
+    @Test
+    public void owl2ImportFromLocal() throws Exception {
+        URI uriOntology = getClass().getClassLoader().getResource("smalltest.owl").toURI();
+        URI uriConfig = getClass().getClassLoader().getResource("smalltest-config.yaml").toURI();
+        runSmallTest(uriOntology.toURL().toString(),uriConfig.toURL().toString());
+    }
+
+    private void runSmallTest(String ontologyUrl, String configUrl) throws KernelException {
         GraphDatabaseService db = setUpDB();
         String call = String.format("CALL ebi.spot.neo4j2owl.owl2Import('%s','%s')",ontologyUrl, configUrl);
 
@@ -61,7 +79,6 @@ public class N2OProcedureTest {
         //assertEquals(new Long(6), db.execute("MATCH (n:ObjectProperty) RETURN count(n) AS count").next().get("count"));
         db.shutdown();
     }
-
 
    @Test
     public void owl2ImportLarge() throws Exception {
