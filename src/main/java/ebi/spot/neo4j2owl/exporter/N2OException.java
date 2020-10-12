0fa85ad68ecfcc7af17dd872f8b8a42dbe245425
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2020-10-12 22:43:36 +0100

    OBO id handling

--- src/main/java/ebi/spot/neo4j2owl/exporter/N2OException.java
+++ src/main/java/ebi/spot/neo4j2owl/exporter/N2OException.java
@@ -1,6 +1,11 @@
 package ebi.spot.neo4j2owl.exporter;
 
 public class N2OException extends Exception {
+
+    public N2OException(String message) {
+        super(message);
+    }
+
     public N2OException(String message, Throwable cause) {
         super(message, cause);
     }
